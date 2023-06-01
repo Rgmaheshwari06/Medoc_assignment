2c7bb92f55daf4e664a08c04f275c37312ea770e
Rishabh <rgmaheshwari06@gmail.com>
2023-06-01 15:01:51 +0530

    Update toss.js

--- toss.js
+++ toss.js
@@ -1,23 +1,23 @@
 function coinToss() {
     // Generate a random number between 0 and 1
-    var randomNum = Math.random();
+    let randomNum = Math.random();
   
     // Assign "Heads" if the number is less than 0.5, otherwise assign "Tails"
-    var result = randomNum < 0.5 ? "Heads" : "Tails";
+    let result = randomNum < 0.5 ? "Heads" : "Tails";
   
     return result;
   }
   
-  var numTosses = parseInt(prompt("Enter the number of coin tosses:"));
+  let numTosses = parseInt(prompt("Enter the number of coin tosses:"));
   
-  var results = [];
-  for (var i = 0; i < numTosses; i++) {
-    var result = coinToss();
+  let results = [];
+  for (let i = 0; i < numTosses; i++) {
+    let result = coinToss();
     results.push(result);
   }
   
   console.log("Coin Toss Results:");
-  for (var i = 0; i < results.length; i++) {
+  for (let i = 0; i < results.length; i++) {
     console.log("Toss " + (i + 1) + ": " + results[i]);
   }
   
